7de23af470b94fd9c74cef3aced0662414ab8538
Yun Do Hyeon <yundohyun050121@gmail.com>
2022-10-18 22:57:19 +0900

    remove: `NPC` Command

--- src/main/java/green/example/custom/command/NPC.kt
+++
@@ -1,9 +0,0 @@
-package green.example.custom.command
-
-import green.example.command.Command
-import green.example.custom.command.sub.npc.NPCSpawn
-
-class NPC: Command(
-    arrayOf(NPCSpawn()),
-    true
-)
